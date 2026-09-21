6b3c2e9d5db93539b75cff18d3b1647d16faf883
Shruti Gupta <sg6392394@gmail.com>
2026-09-21 22:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (13.81%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.cpp
+++ 961-long-pressed-name/long-pressed-name.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
-        if(name.length() > typed.length()) return false;
-        int i = 0;
-        int j = 0;
+        int i=0, j=0;
         while(j < typed.length()){
             if(i < name.length() && name[i] == typed[j]){
-                i++;
+                i++; 
                 j++;
             }
-            else if(j > 0 && typed[j] == typed[j - 1]){
-                j++;
-            }
+            else if(j > 0 && typed[j] == typed[j - 1]) j++;
             else return false;
         }
         return i == name.length();
